d96e01918c18bda46a804ccef47f8540934f763d
cdevjarrell <christian.j.jarrell@gmail.com>
2021-05-16 14:18:29 -0400

    Create navbar.

---
+++ src/Components/Navbar/navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar(props) {
+  return (
+    <div className="row row-spacer">
+      <div className="col-md-12" style={{ passing: 0 }}>
+        <div className="navbar=nav">
+          <h1>Christian's Music Library</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
