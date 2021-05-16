146fd26d43b545ab5a6d183324c46f4c0f892ddb
cdevjarrell <christian.j.jarrell@gmail.com>
2021-05-16 14:21:24 -0400

    Create Music Table.

---
+++ src/Components/MusicTable/musicTable.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+const MusicList = ({ music }) => {
+  return music.map((song, i) => (
+    <table id="song">
+      <tr>
+        <td>{music[i].id}</td>
+        <td>{music[i].title}</td>
+        <td>{music[i].album}</td>
+        <td>{music[i].artist}</td>
+        <td>{music[i].genre}</td>
+      </tr>
+    </table>
+  ));
+};
+
+export default MusicList;
